b909bc7101fdede3ecd70a41a6a40ef043f4ff56
Daniel Chong <danielgq.chong@gmail.com>
2017-02-23 16:50:50 +1300

    able to redirect to editing form

--- App/Components/ExpenseView.js
+++ App/Components/ExpenseView.js
@@ -1,31 +1,69 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
+import { observable, computed } from 'mobx'
+import { observer } from 'mobx-react'
 import Moment from 'moment'
-import { ListItem } from 'react-native-elements'
+import { FormLabel, FormInput } from 'react-native-elements'
+import * as firebase from 'firebase'
 
+import store from '../Model/MainStore.js'
 import styles from './Styles/ExpenseView.style.js'
 
+@observer
 export default class ExpenseView extends Component {
-  render () {
-    let { key, date, description, amount, comment } = this.props.expense
+  @observable date = ''
+  @observable description = ''
+  @observable amount = ''
+  @observable comment = ''
 
-    let dateText = Moment(date).format('DD/MM/YY  HH:mm')
+  constructor (props) {
+    super(props)
+
+    this.date = props.expense.date
+    this.description = props.expense.description
+    this.amount = props.expense.amount
+    this.comment = props.expense.comment
+
+    let userMobilePath = '/user/' + store.userDevice.userId
+      + '/expenses/' + props.expense.key
+    this.expensesRef = firebase.database().ref(userMobilePath)
+  }
+
+  save() {
+    console.log('saving')
+  }
+
+  delete() {
+    console.log('deleting')
+  }
+
+  render () {
+    let dateText = Moment(this.date).format('DD/MM/YY')
+    let timeText = Moment(this.date).format('HH:mm')
 
     return (
-      <ListItem
-        key={key}
-        title={
-          <View style={styles.row}>
-            <Text style={styles.description}>{description}</Text>
-            <Text style={styles.amount}>$ {amount}</Text>
-          </View>
-              }
-        subtitle={
-          <View style={styles.subtitle}>
-            <Text style={styles.date}>{dateText}</Text>
-            <Text style={styles.comment}>{comment}</Text>
-          </View>
-                 } />
+      <View style={styles.container}>
+        <FormLabel>Date</FormLabel>
+        <FormInput value={dateText}/>
+
+        <FormLabel>Time</FormLabel>
+        <FormInput value={timeText}/>
+
+        <FormLabel>Amount</FormLabel>
+        <FormInput value={this.amount}/>
+
+        <FormLabel>Description</FormLabel>
+        <FormInput value={this.description}/>
+
+        <FormLabel>Comment</FormLabel>
+        <FormInput value={this.comment}/>
+
+        <Button title='Save'
+          onPress={() => this.save()} />
+        <Button title='Delete'
+          color='red'
+          onPress={() => this.delete()} />
+      </View>
     )
   }
 }
