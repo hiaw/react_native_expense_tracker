f7959292ed825c39768df62d4a6b4896ceafa1c8
Daniel Chong <danielgq.chong@gmail.com>
2017-02-27 18:57:00 +1300

    more test fixing the date

--- __tests__/Component/Expense/WeekView.test.js
+++ __tests__/Component/Expense/WeekView.test.js
@@ -1,21 +1,21 @@
 import 'react-native'
 import React from 'react'
-import ExpenseRowView from '../../../App/Components/Expenses/ExpenseRowView.js'
+import WeekView from '../../../App/Components/Expenses/WeekView.js'
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
 
-let expense = {
+let data = [{
   _id: 'adsflkja',
-  date: new Date(),
+  date: new Date(2000, 1, 1),
   description: 'description',
   amount: 99.00,
   comment: 'Some long comment'
-}
+}]
 
 it('renders correctly', () => {
   const tree = renderer.create(
-    <ExpenseRowView expense={expense} />
+    <WeekView data={data} week={4} />
   ).toJSON()
   expect(tree).toMatchSnapshot()
 })
